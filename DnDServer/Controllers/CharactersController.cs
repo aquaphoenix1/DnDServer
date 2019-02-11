dac3a1a772c736164d785befa917c88e35b57616
aquaphoenix1 <aqua_phoenix@mail.ru>
2019-02-11 13:40:47 +0400

    1

--- DnDServer/Controllers/CharactersController.cs
+++ DnDServer/Controllers/CharactersController.cs
@@ -1,5 +1,6 @@
 ﻿using DnDServer.Entities;
 using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Web.Http;
 
@@ -20,12 +21,13 @@ namespace DnDServer.Controllers
         {
             try
             {
+                string name = value.UserName;
+
                 string action = value.Action;
                 switch (action)
                 {
                     case "Load":
                         {
-                            string name = value.UserName;
                             if (characters.ContainsKey(name))
                             {
                                 characters.Remove(name);
@@ -37,12 +39,98 @@ namespace DnDServer.Controllers
                         }
                     case "Change":
                         {
+                            string obj = value.ValueName;
+                            switch (obj)
+                            {
+                                case "XP":
+                                    {
+                                        var a = characters[name];
+                                        var b = a.XP;
+                                        b = (int)value.Value;
+                                        break;
+                                    }
+                                case "Current HP":
+                                    {
+                                        break;
+                                    }
+                                case "Time HP":
+                                    {
+                                        break;
+                                    }
+                                case "CheckBox Bone HP":
+                                    {
+                                        break;
+                                    }
+                                case "CheckBox Alive One":
+                                    {
+                                        break;
+                                    }
+                                case "CheckBox Alive Two":
+                                    {
+                                        break;
+                                    }
+                                case "CheckBox Alive Three":
+                                    {
+                                        break;
+                                    }
+                                case "CheckBox Death One":
+                                    {
+                                        break;
+                                    }
+                                case "CheckBox Death Two":
+                                    {
+                                        break;
+                                    }
+                                case "CheckBox Death Three":
+                                    {
+                                        break;
 
+                                    }
+                                case "Copper":
+                                    {
+                                        break;
+                                    }
+                                case "Silver":
+                                    {
+                                        break;
+                                    }
+                                case "Electro":
+                                    {
+                                        break;
+                                    }
+                                case "Gold":
+                                    {
+                                        break;
+                                    }
+                                case "Platinum":
+                                    {
+                                        break;
+                                    }
+                                case "d1":
+                                    {
+                                        //TODO
+                                        break;
+                                    }
+                                case "d2":
+                                    {
+                                        //TODO
+                                        break;
+                                    }
+                                case "d3":
+                                    {
+                                        //TODO
+                                        break;
+                                    }
+                                case "Inspiration":
+                                    {
+                                        break;
+                                    }
+                            }
                             break;
                         }
                 }
             }
-            catch {
+            catch (Exception e){
             }
         }
     }
