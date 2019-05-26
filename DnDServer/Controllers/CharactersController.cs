ccacc3ca15de96bf490406f84e9a13f3da1ec2dd
aquaphoenix1 <aqua_phoenix@mail.ru>
2019-05-26 23:38:14 +0400

    213

--- DnDServer/Controllers/CharactersController.cs
+++ DnDServer/Controllers/CharactersController.cs
@@ -9,11 +9,23 @@ namespace DnDServer.Controllers
     public class CharactersController : ApiController
     {
         private static Dictionary<string, dynamic> characters = new Dictionary<string, dynamic>();
+        private static Dictionary<string, dynamic> lowCharacters = new Dictionary<string, dynamic>();
 
         // GET: api/Characters
-        public string Get()
+        public string Get([FromBody]dynamic name)
         {
+            if(name != null)
+            {
+                return JsonConvert.SerializeObject(characters[name.ToString()]);
+            }
+
             return JsonConvert.SerializeObject(characters);
+        }
+
+        private dynamic LowCharacter(dynamic character)
+        {
+
+            return null;
         }
 
         // POST: api/Characters
@@ -31,97 +43,98 @@ namespace DnDServer.Controllers
                             if (characters.ContainsKey(name))
                             {
                                 characters.Remove(name);
+                                lowCharacters.Remove(name);
                             }
 
                             characters.Add(name, value.Value);
+                            lowCharacters.Add(name, LowCharacter(value.Value));
 
                             break;
                         }
                     case "Change":
                         {
                             string obj = value.ValueName;
+                            var character = characters[name];
                             switch (obj)
                             {
                                 case "XP":
                                     {
-                                        var a = characters[name];
-                                        a.XP = (int)value.Value;
+                                        character.XP = (int)value.Value;
                                         break;
                                     }
                                 case "Current HP":
                                     {
+                                        character.CurrentHP = (int)value.Value;
                                         break;
                                     }
                                 case "Time HP":
                                     {
+                                        character.TimeHP = (int)value.Value;
                                         break;
                                     }
                                 case "CheckBox Bone HP":
                                     {
+                                        character.IsCheckedBoneHP = Boolean.Parse(value.Value.ToString());
                                         break;
                                     }
                                 case "CheckBox Alive One":
                                     {
+                                        character.DeadAndAlive.Alives[0] = Boolean.Parse(value.Value.ToString());
                                         break;
                                     }
                                 case "CheckBox Alive Two":
                                     {
+                                        character.DeadAndAlive.Alives[1] = Boolean.Parse(value.Value.ToString());
                                         break;
                                     }
                                 case "CheckBox Alive Three":
                                     {
+                                        character.DeadAndAlive.Alives[2] = Boolean.Parse(value.Value.ToString());
                                         break;
                                     }
                                 case "CheckBox Death One":
                                     {
+                                        character.DeadAndAlive.Deathes[0] = Boolean.Parse(value.Value.ToString());
                                         break;
                                     }
                                 case "CheckBox Death Two":
                                     {
+                                        character.DeadAndAlive.Deathes[0] = Boolean.Parse(value.Value.ToString());
                                         break;
                                     }
                                 case "CheckBox Death Three":
                                     {
+                                        character.DeadAndAlive.Deathes[0] = Boolean.Parse(value.Value.ToString());
                                         break;
-
                                     }
                                 case "Copper":
                                     {
+                                        character.Copper = (int)value.Value;
                                         break;
                                     }
                                 case "Silver":
                                     {
+                                        character.Silver = (int)value.Value;
                                         break;
                                     }
                                 case "Electro":
                                     {
+                                        character.Electro = (int)value.Value;
                                         break;
                                     }
                                 case "Gold":
                                     {
+                                        character.Gold = (int)value.Value;
                                         break;
                                     }
                                 case "Platinum":
                                     {
-                                        break;
-                                    }
-                                case "d1":
-                                    {
-                                        //TODO
-                                        break;
-                                    }
-                                case "d2":
-                                    {
-                                        //TODO
-                                        break;
-                                    }
-                                case "d3":
-                                    {
-                                        //TODO
+                                        character.Platinum = (int)value.Value;
                                         break;
                                     }
                                 case "Inspiration":
                                     {
+                                        character.Inspiration = Boolean.Parse(value.Value.ToString());
                                         break;
                                     }
                             }
@@ -129,7 +142,8 @@ namespace DnDServer.Controllers
                         }
                 }
             }
-            catch (Exception e){
+            catch (Exception)
+            {
             }
         }
     }
